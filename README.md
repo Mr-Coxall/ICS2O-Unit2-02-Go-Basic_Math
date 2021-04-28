db73a69d19ff5ba820334d00342e170a3d9547b5
Patrick Coxall <mr.coxall@mths.ca>
2021-04-28 23:26:57 +0000

    init

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ICS2O-Unit2-02-Go-Basic_Math
+[![GitHub's Super Linter](https://github.com/Mr-Coxall/ICS2O-Unit2-02-Go-Basic_Math/workflows/GitHub's%20Super%20Linter/badge.svg)](https://github.com/Mr-Coxall/ICS2O-Unit2-02-Go-Basic_Math/actions)
---
+++ basicMath.go
@@ -0,0 +1,22 @@
+/*
+	* Created by: Mr Coxall
+	* Created on: Sep 2020
+	*
+	* This program does basic math
+*/
+
+package main
+
+import (
+ "fmt"
+ "math"
+)
+
+func main() {
+	fmt.Println(9 + 2)
+  fmt.Println("7 - 3 = ", (7 - 3))
+  fmt.Println("4 / 2 = ", (4 / 2))
+  fmt.Println("4 + 4 / 2 = ", (4 + 4 / 2))
+  fmt.Println("3 + 4 * 2 = ", (3 + 4 * 2))
+	fmt.Println("5 + 2³ = ", (5 + math.Pow(2, 3)))
+}
